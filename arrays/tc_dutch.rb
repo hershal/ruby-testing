bf9ccddab969bcbdc763277e75a39969ccde0718
Hershal Bhave <hershal.bhave@gmail.com>
2016-03-19 21:23:02 -0500

    tc_dutch: cleaned up test cases

--- arrays/tc_dutch.rb
+++ arrays/tc_dutch.rb
@@ -3,44 +3,65 @@ require 'test/unit'
 
 # Tests the DutchFlagPartitioner
 class TestDutchFlagPartitioner < Test::Unit::TestCase
+  # -- Test Cases --
+
   def test_simple
     i = 1
     ar = [9, 5, 1, 6, 3]
-    assert_brute(ar, i)
+    check(ar, i)
   end
 
   def test_multiple_equal
     i = 1
     ar = [9, 5, 1, 6, 3, 5]
-    assert_brute(ar, i)
+    check(ar, i)
   end
 
   def test_no_lower
     i = 1
     ar = [1, 5, 1, 2, 3, 4]
-    assert_brute(ar, i)
+    check(ar, i)
   end
 
   def test_no_higher
     i = 1
     ar = [7, 5, 8, 9, 10, 11]
-    assert_brute(ar, i)
+    check(ar, i)
   end
 
-  def assert_brute(ar, i)
-    af = assemble_brute_flag(ar, i)
-    assert(flag?(af, i), "#{af.inspect} not a flag")
+  def test_not_a_flag
+    i = 1
+    ar = [9, 5, 1, 6, 3]
+    assert_notflag(ar, ar[i])
   end
 
-  def assemble_brute_flag(ar, i)
+  # -- Helper Methods --
+
+  def check(ar, i)
+    check_brute(ar, i)
+  end
+
+  def check_brute(ar, i)
     part = DutchFlagPartitioner.new(ar, i)
-    part.arrange_brute
+    ai = ar[i]
+    af = part.arrange_brute
+    assert_flag(af, ai)
   end
 
-  def flag?(af, i)
-    ai = af[i]
-    af.each_with_index do |aa, idx|
-      return false if (idx < i && aa > ai) || (idx > i && aa < ai)
+  def assert_flag(af, ai)
+    assert(flag?(af, ai), "#{af.inspect} not a flag")
+  end
+
+  def assert_notflag(af, ai)
+    assert(!flag?(af, ai), "#{af.inspect} is a flag (test for notflag)")
+  end
+
+  def flag?(af, ai)
+    crossed_pivot = false
+    af.each do |aa|
+      crossed_pivot ||= (aa == ai)
+      return false if (!crossed_pivot && (aa > ai)) ||
+                      (crossed_pivot && (aa < ai))
     end
     true
   end
