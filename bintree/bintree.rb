b5123baecc2ca9391c80331874107d8bbb6a5c6d
Hershal Bhave <hershal.bhave@gmail.com>
2016-03-21 00:01:24 -0500

    bintree: added basic binary tree implementation

---
+++ bintree/bintree.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+# A sorted binary tree which contains a reference to the root of the tree and
+# algorithms to operate on the tree.
+class BinaryTree
+  def initialize
+    @root = nil
+  end
+
+  # Publicly accessable node adder
+  def add(data)
+    if @root.nil?
+      @root = BinaryNode.new(data)
+    else
+      _add(data, @root)
+    end
+  end
+
+  # implementation specific node adder
+  def _add(data, root)
+    if data < root.data
+      if root.left.nil?
+        root.left = BinaryNode.new(data)
+      else
+        _add(data, root.left)
+      end
+    else
+      if root.right.nil?
+        root.right = BinaryNode.new(data)
+      else
+        _add(data, root.right)
+      end
+    end
+  end
+
+  def to_a
+    _to_a(@root)
+  end
+
+  def _to_a(root)
+    return if root.nil?
+    [_to_a(root.left), root.data, _to_a(root.right)].compact.flatten
+  end
+end
+
+# Encapsulates a binary node, which contains some data as well as references to
+# its left and right children.
+class BinaryNode
+  attr_accessor :left, :right
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+end
---
+++ bintree/tc_add.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require_relative 'bintree'
+require 'test/unit'
+
+# Tests the BinaryTree Add implementation
+class TestBinaryTreeAdd < Test::Unit::TestCase
+  def test_simple_0
+    arr = [2, 1, 3]
+    check(arr, assemble(arr))
+  end
+
+  def test_simple_1
+    arr = [4, 2, 6, 1, 3, 5, 7]
+    check(arr, assemble(arr))
+  end
+
+  def assemble(data)
+    tree = BinaryTree.new
+    data.each do |e|
+      tree.add(e)
+    end
+    tree
+  end
+
+  def check(arr, tree)
+    expected = arr.sort
+    actual = tree.to_a
+    assert(expected == actual, "#{expected} ne #{actual}")
+  end
+end
